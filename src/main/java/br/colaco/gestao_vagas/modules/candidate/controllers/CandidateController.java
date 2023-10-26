a67ffbb9985a2dd17f074aa5ab53655728a1006d
bruno <bruno@colaco.com.br>
2023-10-26 00:29:53 -0300

    Ajuste de import

--- src/main/java/br/colaco/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/colaco/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -7,9 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.colaco.gestao_vagas.exceptions.UserFoundException;
 import br.colaco.gestao_vagas.modules.candidate.CandidateEntity;
-import br.colaco.gestao_vagas.modules.candidate.CandidateRepository;
 import br.colaco.gestao_vagas.modules.candidate.useCases.CreateCandidateUseCase;
 import jakarta.validation.Valid;
 
