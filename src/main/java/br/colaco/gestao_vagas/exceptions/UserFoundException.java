1148b6d26669c6a1ea2fd8586a62fd2b0851d9ff
bruno <bruno@colaco.com.br>
2023-10-26 00:08:37 -0300

    Validando usuário já existente

---
+++ src/main/java/br/colaco/gestao_vagas/exceptions/UserFoundException.java
@@ -0,0 +1,8 @@
+package br.colaco.gestao_vagas.exceptions;
+
+public class UserFoundException extends RuntimeException {
+  public UserFoundException() {
+    super("Usuário já existe.");
+  }
+  
+}
